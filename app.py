8ae14349ed6c10c9a8ec602a6c50f8eab4fce6b7
Angela Julie Dobbs <ajdobbs.sm@gmail.com>
2016-08-17 11:48:18 -0400

    Set up the basic api with create user endpoint

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
+# from flask import Flask
+# app = Flask(__name__)
+#
+# @app.route("/")
+# def main():
+#     return "Welcome!"
+#
+# if __name__ == "__main__":
+#     app.run()
+
 from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
 app = Flask(__name__)
+api = Api(app)
+parser = reqparse.RequestParser()
+parser.add_argument('email', type=str, help='Email address to create user')
+parser.add_argument('password', type=str, help='Password to create user')
 
-@app.route("/")
-def main():
-    return "Welcome!"
+class CreateUser(Resource):
+    def post(self):
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('email', type=str, help='Email address to create user')
+            parser.add_argument('password', type=str, help='Password to create user')
+            args = parser.parse_args()
 
-if __name__ == "__main__":
-    app.run()
+            _userEmail = args['email']
+            _userPassword = args['password']
+
+            return {'Email': args['email'], 'Password': args['password']}
+
+        except Exception as e:
+            return {'error': str(e)}
+
+api.add_resource(CreateUser, '/CreateUser')
+
+if __name__ == '__main__':
+    app.run(debug=True)
