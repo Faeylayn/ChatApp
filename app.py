f5f909a867b5666046f4259616ec2c46507e0aef
Angela Julie Dobbs <ajdobbs.sm@gmail.com>
2016-08-18 19:02:37 -0400

    Added the db and a reasonable set up for sql alchemy

--- app.py
+++ app.py
@@ -10,30 +10,92 @@
 
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
+import sqlite3
+from flask import g
+# from flask.ext.mysql import MySQL
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'ChatUser'
+    id = Column(Integer, primary_key=True)
+    UserName = Column(String(250), nullable=False)
+
+# class Message(Base):
+#     __tablename__ = 'address'
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
+
+# Create an engine that stores data in the local directory's
+# sqlalchemy_example.db file.
+engine = create_engine('sqlite:///chatapp.db')
+Base.metadata.create_all(engine)
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+
+session = DBSession()
+
+# Insert a Person in the person table
+# new_person = Person(name='new person')
+# session.add(new_person)
+# session.commit()
+
 
 app = Flask(__name__)
 api = Api(app)
-parser = reqparse.RequestParser()
-parser.add_argument('email', type=str, help='Email address to create user')
-parser.add_argument('password', type=str, help='Password to create user')
 
-class CreateUser(Resource):
+DATABASE = './chatapp.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+# app.config['MYSQL_DATABASE_USER'] = 'root'
+# app.config['MYSQL_DATABASE_PASSWORD'] = 'root123'
+# app.config['MYSQL_DATABASE_DB'] = 'ChatAppDB'
+# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+# mysql.init_app(app)
+# conn = mysql.connect()
+
+
+class LoginUser(Resource):
     def post(self):
         try:
+            # cur = get_db().cursor()
             parser = reqparse.RequestParser()
-            parser.add_argument('email', type=str, help='Email address to create user')
-            parser.add_argument('password', type=str, help='Password to create user')
+            parser.add_argument('email', type=str, help='Email address to lookup/create user')
             args = parser.parse_args()
 
             _userEmail = args['email']
-            _userPassword = args['password']
+            new_person = User(UserName=_userEmail)
+            session.add(new_person)
+            session.commit()
 
-            return {'Email': args['email'], 'Password': args['password']}
+            return {'Email': _userEmail}
 
         except Exception as e:
             return {'error': str(e)}
 
-api.add_resource(CreateUser, '/CreateUser')
+api.add_resource(LoginUser, '/LoginUser')
 
 if __name__ == '__main__':
     app.run(debug=True)
